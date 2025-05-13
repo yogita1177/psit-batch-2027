93b1a6ad6891e382c48876a9ef7c270ea8b3655c
Aakash Verma <info.aakash11@gmail.com>
2025-05-13 18:42:04 +0530

    react fundamentals may 13

--- advanced-javascript/hello.js
+++ advanced-javascript/hello.js
@@ -1,68 +1,74 @@
-class Car {
-    static wheels = 4;
-    constructor(
-      color,
-      wheelsColor,
-      brand,
-      engine
-    ) {
-      this.color = color;
-      this.wheelsColor = wheelsColor;
-      this.brand = brand;
-      this.engine = engine;
-    }
-  }
+// class Car {
+//     static wheels = 4;
+//     constructor(
+//       color,
+//       wheelsColor,
+//       brand,
+//       engine
+//     ) {
+//       this.color = color;
+//       this.wheelsColor = wheelsColor;
+//       this.brand = brand;
+//       this.engine = engine;
+//     }
+//   }
   
-  class ElectricCar extends Car {
-    constructor(
-      color,
-      wheelsColor,
-      brand,
-      engine,
-      batteryBackup,
-      chargingTime
-    ) {
-      this.batteryBackup = batteryBackup;
-      this.chargingTime = chargingTime;
-      super(color, wheelsColor, brand, engine);
-    }
-  }
+//   class ElectricCar extends Car {
+//     constructor(
+//       color,
+//       wheelsColor,
+//       brand,
+//       engine,
+//       batteryBackup,
+//       chargingTime
+//     ) {
+//       this.batteryBackup = batteryBackup;
+//       this.chargingTime = chargingTime;
+//       super(color, wheelsColor, brand, engine);
+//     }
+//   }
   
-  class PetrolCar extends Car {
-    constructor(
-      color,
-      wheelsColor,
-      brand,
-      engine,
-      milaege
-    ) {
-      this.milaege = milaege;
-      super(color, wheelsColor, brand, engine);
-    }
-  }
+//   class PetrolCar extends Car {
+//     constructor(
+//       color,
+//       wheelsColor,
+//       brand,
+//       engine,
+//       milaege
+//     ) {
+//       this.milaege = milaege;
+//       super(color, wheelsColor, brand, engine);
+//     }
+//   }
   
-  var json = {
-    color: "White",
-    wheelsColor: "Black",
-    brand: "Tesla",
-    engine: 3000,
-    batteryBackup: "450Kms",
-    chargingTime: "2 hours",
-    type: "ELECTRIC",
-  };
+//   var json = {
+//     color: "White",
+//     wheelsColor: "Black",
+//     brand: "Tesla",
+//     engine: 3000,
+//     batteryBackup: "450Kms",
+//     chargingTime: "2 hours",
+//     type: "ELECTRIC",
+//   };
   
-  if (json.type === "ELECTRIC") {
-    var car1 = new ElectricCar(
-      json.color,
-      json.wheelsColor,
-      json.brand,
-      json.engine,
-      json.batteryBackup,
-      json.chargingTime
-    );
-    console.log(car1);
-  } else {
-    var car2 = new PetrolCar("White", "Black", "Tesla", 3000, 18);
-    console.log(car2);
-  }
-  
+//   if (json.type === "ELECTRIC") {
+//     var car1 = new ElectricCar(
+//       json.color,
+//       json.wheelsColor,
+//       json.brand,
+//       json.engine,
+//       json.batteryBackup,
+//       json.chargingTime
+//     );
+//     console.log(car1);
+//   } else {
+//     var car2 = new PetrolCar("White", "Black", "Tesla", 3000, 18);
+//     console.log(car2);
+//   }
+
+
+export function hello() {
+  return <h1>Hello</h1>;
+}
+
+hello()
---
+++ react/hello-world-react-app/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export function App() {
+  return <h1 id="heading" className="heading">Hello, I am coming from App.js file.</h1>;
+
+  // return React.createElement("h1", { id: "heading", className: "heading" }, "Hello, I am coming from App.js file via React element.");
+}
