6800a38d2d2222ec45ab868a58ca18065f1ad777
Aakash Verma <info.aakash11@gmail.com>
2025-06-03 18:38:29 +0530

    updated june 3

--- react/ecommerce-react/src/components/ListProducts.jsx
+++ react/ecommerce-react/src/components/ListProducts.jsx
@@ -1,22 +1,65 @@
 import ProductCard from './ProductCard';
-import products from '../data/products';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function ListProducts() {
   //   let productList = products;
-  const [productList, setProductList] = useState(products);
+  const [productList, setProductList] = useState([]);
+  const [filteredProductList, setFilteredProductList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  async function fetchProducts() {
+    const response = await fetch(
+      'https://683ee8691cd60dca33dd8740.mockapi.io/api/v1/products'
+    );
+    const products = await response.json();
+    setProductList(products);
+    setFilteredProductList(products);
+    return products;
+  }
 
   function filterProducts() {
-    setProductList(products.filter((product) => product.price > 200000));
+    setFilteredProductList(
+      productList.filter((product) => product.price > 200000)
+    );
   }
+
+  function resetProduct() {
+    setFilteredProductList(productList);
+  }
+
+  function searchProducts() {
+    setFilteredProductList(
+      productList.filter((product) =>
+        product.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    );
+  }
+
   return (
     <section className='products'>
       <h1>Trending Products</h1>
+      <input
+        type='text'
+        value={searchQuery}
+        placeholder='Search for products...'
+        onChange={(event) => setSearchQuery(event.target.value)}
+        className='search-products'
+      />
+      <button onClick={searchProducts} className='search-product-btn'>
+        Search
+      </button>
       <button onClick={filterProducts} className='filter-btn'>
         Filter Products Above 2000
       </button>
+      <button onClick={resetProduct} className='reset-btn'>
+        Reset
+      </button>
       <div className='products-grid'>
-        {productList.map((product) => (
+        {filteredProductList.map((product) => (
           <ProductCard
             key={product.id}
             productDetails={{
